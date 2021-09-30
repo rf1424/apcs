b14349782af5dd3ed46d1bbc438384c50226164a
rf1424 <rfukuoka30@stuy.edu>
2021-09-30 19:51:17 -0400

    update

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,10 @@
+public class Greet {
+public static void greet(String name) {
+System.out.println("Why, hello there, " + name + " How do you do?");
+}
+public static void main(String[] args) {
+greet("Julia");
+greet("Ethan");
+greet("Mr. Mykolyk");
+} 
+}
