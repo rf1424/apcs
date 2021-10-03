3a986a1fd8ec90d4deaa6f26d229946af78fd9fd
rf1424 <rfukuoka30@stuy.edu>
2021-10-03 16:01:17 -0400

    hw09

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,12 @@
+/*
+Rin Fukuoka
+APCS
+hw09
+*/
+
+
+public class BigSib {
+public static void greet(String name) {
+System.out.println("Why, hello there, " + name + ". How do you do?");
+}
+}
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,15 @@
+/*
+Rin Fukuoka
+APCS
+hw09
+*/
+
+
+public class Greet {
+    public static void main( String[] args ) {
+        BigSib.greet("Flim");
+        BigSib.greet("Flam");
+        BigSib.greet("Kazaam");
+    }
+}
+
