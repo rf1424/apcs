194d2169ae97b9a971930943bb0beced7618fe6c
rf1424 <rfukuoka30@stuy.edu>
2021-10-04 22:16:23 -0400

    hw10

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,14 @@
+/*
+Elmo's Minions: 
+Rin Fukuoka and Tape
+Joshua Yagupsky and Quacker
+Marcus Wu and Nat 
+APCS
+hw09
+*/
+
+public class BigSib {
+public static void greet(String name) {
+System.out.println("Why, hello there, " + name + ". How do you do?");
+}
+}
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,16 @@
+/*
+Elmo's Minions: 
+Rin Fukuoka (Tape), Joshua Yagupsky (Quacker), Marcus Wu (Nat) 
+APCS
+hw09
+2021-10-4
+*/
+
+
+public class Greet {
+    public static void main( String[] args ){
+        BigSib.greet("Crosby");
+        BigSib.greet("Stills");
+        BigSib.greet("Nash");
+    }
+}
