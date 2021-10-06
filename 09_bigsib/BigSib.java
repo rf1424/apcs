3fd7040993a656b198bb002ed1ef992ebf158647
Rin Fukuoka <90658562+rf1424@users.noreply.github.com>
2021-10-05 21:34:37 -0400

    hw09 edit

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,11 +1,13 @@
 /*
 Elmo's Minions: 
-Rin Fukuoka and Tape
-Joshua Yagupsky and Quacker
-Marcus Wu 
-
+Rin Fukuoka (Tape), Joshua Yagupsky (Quacker), Marcus Wu (Nat)
 APCS
 hw09
+*/
+
+/*
+DISCOVERIES: If you previously made edits on GitHub, then "git pull" on terminal before performing "git pull". 
+UNRESOLVED QUESTIONS: Why do you put the Greet.java code and the BigSib.java code in separate files, rather than in a single file?
 */
 
 
