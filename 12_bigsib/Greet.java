4512f6d46c884c8b15fd55767780b70426b64ebd
Rin Fukuoka <90658562+rf1424@users.noreply.github.com>
2021-10-05 21:57:54 -0400

    hw12

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -3,18 +3,30 @@ Elmo's Minions:
 Rin Fukuoka (Tape), Joshua Yagupsky (Quacker), Marcus Wu (Nat) 
 APCS
 hw09
-2021-10-4
+2021-10-05
 */
-
 
 public class Greet {
   public static void main( String[] args ) {
     String greeting;
 
     BigSib richard = new BigSib();
+    
     richard.setHelloMsg("Word up");
 
     greeting = richard.greet("freshman");
+    System.out.println(greeting);
+    
+    richard.setHelloMsg("Salutations");
+    greeting = richard.greet("Dr. Spaceman");
+    System.out.println(greeting);
+    
+    richard.setHelloMsg("Hey ya");
+    greeting = richard.greet("Kong Fooey");
+    System.out.println(greeting);
+    
+    richard.setHelloMsg("Sup");
+    greeting = richard.greet("mom");
     System.out.println(greeting);
 
   } //end main()
