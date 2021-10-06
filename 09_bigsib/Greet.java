d8e4d9f51878bc5d53de8a8d2a493482812f6871
Rin Fukuoka <90658562+rf1424@users.noreply.github.com>
2021-10-05 21:36:19 -0400

    hw09 edit

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,9 +1,15 @@
 /*
-Rin Fukuoka
+Elmo's Minions: 
+Rin Fukuoka (Tape), Joshua Yagupsky (Quacker), Marcus Wu (Nat)
 APCS
 hw09
+2021-10-03
 */
 
+/*
+DISCOVERIES: If you previously made edits on GitHub, then "git pull" on terminal before performing "git push". 
+UNRESOLVED QUESTIONS: Why do you put the Greet.java code and the BigSib.java code in separate files, rather than in a single file?
+*/
 
 public class Greet {
     public static void main( String[] args ) {
