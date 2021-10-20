496ef4c8ff990988185e8b2c26511731835cdb84
Rin Fukuoka <90658562+rf1424@users.noreply.github.com>
2021-10-19 21:52:47 -0400

    Update Teller.java

--- 20/Teller.java
+++ 20/Teller.java
@@ -1,11 +1,19 @@
 public class Teller {
   public static void main (String [] args) {
+
+//test functionality
     BankAccount hacker = new BankAccount ();
     hacker.setName("Goose");
     hacker.setPasswd("whitehat122");
-		hacker.setPin((short) 1254);
-		hacker.setAcctNum(712234456);
 		hacker.setBalance(20000);
+    
+    //test acctNum
+    hacker.setAcctNum(123);//expecting invalid
+		hacker.setAcctNum(712234456);//expecting valid
+    
+    //test PIN
+    hacker.setPin((short) 999)) //expecting invalid
+    hacker.setPin((short) 1254); //expecting valid: 
     
     //test toString(printing info)
     System.out.println( hacker.toString());
@@ -15,6 +23,20 @@ public class Teller {
     System.out.println( hacker.toString());
     
     //test withdraw
+    hacker.withdraw(100.00);
 		hacker.withdraw(120000); //expecting invalid
+    System.out.println(hacker.toString());
+    
+    //test authenticate
+    hacker.authenticate(712234456, "whitehat122"); //expecting true
+    hacker.authenticate(124129481, "getmein"); //expecting false
+  
+//break BankAccount
+    //test withdraw
+   
+    
   }
 }
+
+  
+
