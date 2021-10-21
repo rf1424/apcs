d141a6e4012f2b598226215e1204cad7c86c9595
Rin Fukuoka <90658562+rf1424@users.noreply.github.com>
2021-10-21 01:59:54 -0400

    Update Stats.java

--- 21_Stats/Stats.java
+++ 21_Stats/Stats.java
@@ -10,22 +10,22 @@
  **/
 
 
-public class Stats {
+import java.lang.Math;
 
+public class Stats {
+  
   public static int mean(int a, int b) {
     int mean;
     mean = (a + b)/2;
     return mean;
 
   }
-   
-   
+    
    public static double mean(double a, double b) {
      double mean; 
      mean = a/2 + b/2;
      return mean;
   }
-
 
   public static int max(int a, int b) {
     int max;
@@ -35,9 +35,9 @@ public class Stats {
     else {
       max = b;
       }
-    return max;      
+    return max;
+      
   }
-
   public static double max(double a, double b) {
     double max;
     if (a >= b) { 
@@ -49,34 +49,41 @@ public class Stats {
     return max;
   }
   
-/*
+
   public static int geoMean(int a, int b) {
-
+    int geoMean1;
+    if(a*b>=0){
+    	geoMean1 = Math.round(Math.round(Math.sqrt(a + b)));
+      return geoMean1;
+    }
+    else{
+      System.out.print("undefined");
+      return 0;
+    }
   }
-
   public static double geoMean(double a, double b) {
-
-  }
-
-  public static int max(int a, int b, int c) {
-
-  }
-
-  public static double max(double a, double b, double c) {
-
+    double geoMean2;
+    if (a*b>=0){
+      geoMean2 = Math.sqrt(a + b);
+      return geoMean2;
+    }
+    else{
+      System.out.println("undefined");
+      return 0;
+    }
   }
 
   public static int geoMean(int a, int b, int c) {
-
+    int geoMean3;
+    geoMean3= Math.round(Math.round(Math.cbrt(a*b*c)));
+    return geoMean3;
   }
-
   public static double geoMean(double a, double b, double c) {
-
+    double geoMean4;
+    geoMean4 = Math.cbrt(a * b * c);
+    return geoMean4;
   }
-*/
 
-  //main method for testing functionality
-  public static void main( String[] args ) {
-  }
+
 
 }//end class
