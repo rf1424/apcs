8a623ecf4de3eac8a53656984f25d207a3c248b0
Rin Fukuoka <90658562+rf1424@users.noreply.github.com>
2021-10-21 00:04:41 -0400

    Update Stats.java

--- 21_Stats/Stats.java
+++ 21_Stats/Stats.java
@@ -9,6 +9,7 @@
    arithmetic mean of the reciprocals
  **/
 
+
 public class Stats {
 
   public static int mean(int a, int b) {
@@ -17,7 +18,43 @@ public class Stats {
     return mean;
 
   }
- 
+   
+   
+   public static double mean(double a, double b) {
+
+  }
+
+  public static int max(int a, int b) {
+
+  }
+
+  public static double max(double a, double b) {
+
+  }
+
+  public static int geoMean(int a, int b) {
+
+  }
+
+  public static double geoMean(double a, double b) {
+
+  }
+
+  public static int max(int a, int b, int c) {
+
+  }
+
+  public static double max(double a, double b, double c) {
+
+  }
+
+  public static int geoMean(int a, int b, int c) {
+
+  }
+
+  public static double geoMean(double a, double b, double c) {
+
+  }
   //main method for testing functionality
   public static void main( String[] args ) {
   }
