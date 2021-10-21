e27bae5bcf6bfb5e2faf9cdf8f09cd56ba7fec7b
Rin Fukuoka <90658562+rf1424@users.noreply.github.com>
2021-10-21 00:10:38 -0400

    Update StatsDriver.java

--- 21_Stats/StatsDriver.java
+++ 21_Stats/StatsDriver.java
@@ -3,24 +3,25 @@ public class StatsDriver {
   public static void main(String[]args){
     System.out.println(Stats.mean(2,4));//expected output 3
 
-/*  Stats.mean(3.3);//expected output 3
-    Stats.mean(0,0);//expected output 0
-    Stats.mean(-4,4);//expected output 0
+    System.out.println(Stats.mean(3.3));//expected output 3
+    System.out.println(Stats.mean(0,0));//expected output 0
+    System.out.println(Stats.mean(-4,4));//expected output 0
     
-    Stats.mean(24.50,26.50);//expected ouput 25.50
-    Stats.mean(12.00,14.00);//expected output 13.00
-    Stats.mean(0.00, 100.00);//expected output 50.00
-    Stats.mean(-12.00,-10.00);//expected output -11.00
+    System.out.println(Stats.mean(24.50,26.50));//expected ouput 25.50
+    System.out.println(Stats.mean(12.00,14.00));//expected output 13.00
+    System.out.println( Stats.mean(0.00, 100.00));//expected output 50.00
+    System.out.println(Stats.mean(-12.00,-10.00));//expected output -11.00
+  
+/*   
+    System.out.println(Stats.max(15,20));//expected output 20
+    System.out.println(Stats.max(90,90);//expected output 90
+    System.out.println(Stats.max(-10,-5);//expected output -5
+    System.out.println(Stats.max(0,-4);//expected output 0
     
-    Stats.max(15,20);//expected output 20
-    Stats.max(90,90);//expected output 90
-    Stats.max(-10,-5);//expected output -5
-    Stats.max(0,-4);//expected output 0
-    
-    Stats.max(11.236,376.124);//expected output 376.124
-    Stats.max(1.00,1.00);//expected output 1.00
-    Stats.max(-9.00,-24.000);//expected output -9.00
-    Stats.max(0.000,0.00);//expected output 0.000
+    System.out.println(Stats.max(11.236,376.124));//expected output 376.124
+    System.out.println(Stats.max(1.00,1.00));//expected output 1.00
+    System.out.println(Stats.max(-9.00,-24.000));//expected output -9.00
+    System.out.println(Stats.max(0.000,0.00));//expected output 0.000
     
     Stats.geoMean(4,9);//expected output 6
     Stats.geoMean(0,0);//expected output 0
