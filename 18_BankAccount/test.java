db2cb1771048222da9d8ea7b72310ddb897e7bd5
rf1424 <rfukuoka30@stuy.edu>
2021-10-21 00:01:33 -0400

    Mean

---
+++ 18_BankAccount/test.java
@@ -0,0 +1,11 @@
+public class test{
+  static String fullName;
+  static String password;
+  static float pin;
+  static int accountNumber;
+  static double accountBalance;
+
+public static void main(String[] args){
+    System.out.println(fullName + " " + password + accountNumber + accountBalance + pin);
+}
+}
---
+++ 21_Stats/Stats.java
@@ -0,0 +1,25 @@
+/**
+   Duck Duck Goose: Gloria Lee (Flounder), Jonathan Song (Phoenix), Rin Fukuoka (Tape)
+   APCS
+   HW20 --
+   2021-10-21
+   TASK:
+   Implement each method without typecasting.
+   xtra: harmonic mean is the reciprocal of the
+   arithmetic mean of the reciprocals
+ **/
+
+public class Stats {
+
+  public static int mean(int a, int b) {
+    int mean;
+    mean = a/2 + b/2;
+    return mean;
+
+  }
+ 
+  //main method for testing functionality
+  public static void main( String[] args ) {
+  }
+
+}//end class
